12cd7790c63a797e22ce3f258144d85c326958d7
Briek wozniak <Briek.Wozniak@gmail.com>
2026-03-04 12:40:15 +0100

    feat(calendar): support specific start and end times for events

--- models/Event.js
+++ models/Event.js
@@ -5,6 +5,8 @@ const Event = sequelize.define('Event', {
   title: { type: DataTypes.STRING, allowNull: false },
   date: { type: DataTypes.DATEONLY, allowNull: false },
   endDate: { type: DataTypes.DATEONLY, allowNull: true },
+  startTime: { type: DataTypes.STRING, allowNull: true }, // Format "HH:mm"
+  endTime: { type: DataTypes.STRING, allowNull: true },   // Format "HH:mm"
   description: { type: DataTypes.TEXT }
 });
 
